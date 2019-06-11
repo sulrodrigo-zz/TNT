8cb361b56a2a2ee4cfac92f1845455ba063f1283
RODRIGO EVARISTO DE SUL <rodrigo.sul@ons.org.br>
2019-06-11 14:29:12 -0300

    Adding App Info

--- TNT/Program.cs
+++ TNT/Program.cs
@@ -6,7 +6,19 @@ namespace TNT
     {
         static void Main(string[] args)
         {
-            
+            string AppName = "TNT";
+            string AppVersion = "0.0.1";
+            string AppAuthor = "Rodrigo Sul";
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("{0}: Version {1}. Created By: {2}", AppName, AppVersion, AppAuthor);
+            Console.ResetColor();
+
+            Console.WriteLine("Hey, Newbie! Tell me your name!");
+            string player = Console.ReadLine();
+            Console.WriteLine("Welcome {0}! Let's Play a Game...", player);
+
+            Console.ReadKey();
         }
     }
 }
