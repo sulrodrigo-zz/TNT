7dd0808f30d23978cdd5e35a3a10f473514c2997
RODRIGO EVARISTO DE SUL <rodrigo.sul@ons.org.br>
2019-06-14 17:39:17 -0300

    Adição de Menu

--- TNT/Program.cs
+++ TNT/Program.cs
@@ -9,60 +9,75 @@ namespace TNT
             GetAppInfo();
             GreetUser();
 
+            GameMenu();
+            int choice = int.Parse(Console.ReadLine());
 
-            while (true) {
+            Console.Clear();
+            GetAppInfo();
 
-                Random random = new Random();
-
-                int correctNumber = random.Next(1,10);
-                int guess = 0;
-
-                Console.WriteLine("Choose a Number Between 1 and 10");
-
-                while(guess != correctNumber)
+            if (choice == 1) {
+                while (true)
                 {
-                    string input = Console.ReadLine();
 
-                    if(!int.TryParse(input, out guess))
+                    Random random = new Random();
+
+                    int correctNumber = random.Next(1, 10);
+                    int guess = 0;
+
+                    Console.WriteLine("Choose a Number Between 1 and 10");
+
+                    while (guess != correctNumber)
                     {
-                        PrintColorMSG(ConsoleColor.Red, "Whoops... This is not a number...");
+                        string input = Console.ReadLine();
+
+                        if (!int.TryParse(input, out guess))
+                        {
+                            PrintColorMSG(ConsoleColor.Red, "Whoops... This is not a number...");
+                            continue;
+                        }
+
+                        guess = Int32.Parse(input);
+
+                        if (guess != correctNumber)
+                        {
+                            PrintColorMSG(ConsoleColor.Red, "WRONG NUMBER! Try Again!");
+                        }
+                    }
+                    PrintColorMSG(ConsoleColor.Yellow, "RIGHT NUMBER! Well Done!");
+
+
+                    Console.WriteLine("Play Again? [Y or N]");
+                    string answer = Console.ReadLine().ToUpper();
+                    if (answer == "Y")
+                    {
                         continue;
                     }
-
-                    guess = Int32.Parse(input);
-
-                    if(guess != correctNumber)
+                    else if (answer == "N")
                     {
-                        PrintColorMSG(ConsoleColor.Red, "WRONG NUMBER! Try Again!");
+                        Console.WriteLine("Bye!");
+                        Console.ReadKey();
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine("You're Badass, I like that. No Rules!");
+                        Console.ReadKey();
+                        return;
                     }
                 }
-
-                PrintColorMSG(ConsoleColor.Yellow, "RIGHT NUMBER! Well Done!");
-
-                Console.WriteLine("Play Again? [Y or N]");
-                string answer = Console.ReadLine().ToUpper();
-                if(answer == "Y")
-                {
-                    continue;
-                }
-                else if (answer == "N")
-                {
-                    Console.ReadKey();
-                    return;
-                }
-                else
-                {
-                    Console.ReadKey();
-                    return;
-                }                
+            }
+            else
+            {
+                PrintColorMSG(ConsoleColor.Red, "Not a Game!");
+                Console.ReadKey();
+                return;
             }
         }
-
         // Display App Info
         static void GetAppInfo()
         {
             string AppName = "TNT";
-            string AppVersion = "0.1.0";
+            string AppVersion = "0.2.0";
             string AppAuthor = "Rodrigo Sul";
 
             Console.ForegroundColor = ConsoleColor.Green;
@@ -94,5 +109,16 @@ namespace TNT
             Console.ResetColor();
         }
 
+        static void GameMenu()
+        {
+            PrintColorMSG(ConsoleColor.Blue,"        :: Choose Your Game! ::");
+            Console.WriteLine(@"   _______________________________   ");
+            Console.WriteLine(@"   |                             |   ");
+            Console.WriteLine(@"   |    [1] - Number Guesser     |   ");
+            Console.WriteLine(@"   |                             |   ");
+            Console.WriteLine(@"   |                             |   ");
+            Console.WriteLine(@"   |                             |   ");
+            Console.WriteLine(@"   |_____________________________|   ");
+        }
     }
 }
